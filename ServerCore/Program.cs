08191fd562153a93d35e69d6a552cdedb8920545
OhJaeYoung <ostyoung@naver.com>
2023-02-12 17:33:23 +0900

    Memory Barrier

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -101,9 +101,95 @@ namespace ServerCore
             }
         }
         #endregion
+
+        #region Memory Barrier
+        // 메모리 베리어
+        // A) 코드 재배치 억제
+        // B) 가시성
+        //                        어셈블리언어                            값을 저장/ 값을 끄집어내는것
+        // 1) Full Memory Barrier (ASM MFENCE, C# Thread.MemoryBarrier) : Store / Load 둘다 막는다
+        // 2) Store Memeory Barrier (ASM SFENCE) : Store만 막는다
+        // 3) Load Memeory Barrier (ASM LFENCE) : Load만 막는다
+        static int x = 0;
+        static int y = 0;
+        static int r1 = 0;
+        static int r2 = 0;
+        static void MemoryBarrier1()
+        {
+            int count = 0;
+            while (true)
+            {
+                count++;
+                x = y = r1 = r2 = 0;
+
+                Task t1 = new Task(Thread_1);
+                Task t2 = new Task(Thread_2);
+                t1.Start();
+                t2.Start();
+
+                Task.WaitAll(t1, t2);
+
+                if (r1 == 0 && r2 == 0)
+                    break;
+            }
+
+            Console.WriteLine($"{count}번 만에 빠져나옴!");
+        }
+
+        static void Thread_1()
+        {
+            y = 1; // Store y
+
+            // ----------------------------
+            Thread.MemoryBarrier();
+            
+            r1 = x; // Load x
+        }
+
+        static void Thread_2()
+        {
+            x = 1;  // Store x
+
+            // ----------------------------
+            Thread.MemoryBarrier();
+
+            r2 = y; // Load y
+        }
+
+        static int _answer;
+        static bool _complete;
+
+        static void MemoryBarrier2()
+        {
+            Task t1 = new Task(A);
+            Task t2 = new Task(B);
+
+            t1.Start();
+            t2.Start();
+
+            Task.WaitAll(t1, t2);
+        }
+        static void A()
+        {
+            _answer = 123;
+            Thread.MemoryBarrier();
+            _complete = true;
+            Thread.MemoryBarrier();
+        }
+        static void B()
+        {
+            Thread.MemoryBarrier();
+            if (_complete)
+            {
+                Thread.MemoryBarrier();
+                Console.WriteLine(_answer);
+            }
+        }
+
+        #endregion
         static void Main(string[] args)
         {
-            Cache();
+
         }
     }
 }
