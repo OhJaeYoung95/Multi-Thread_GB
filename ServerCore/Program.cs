3d9eaa92df6849115c6f81dd44b83d680311dd4f
OhJaeYoung <ostyoung@naver.com>
2023-02-15 18:39:37 +0900

    Context Switching

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -27,6 +27,13 @@ namespace ServerCore
                 int desired = 1;
                 if( (Interlocked.CompareExchange(ref _locked, desired, expected)) == expected )
                     break;
+
+                // 쉬다 올게~
+                // Thread.Sleep(1);    // 무조건 휴식 => 무조건 1ms 정도 쉰다(시간은 운영체제가 대략적으로 비슷하도록 결정해줌)
+                // Thread.Sleep(0);    // 조건부 양보 => 나보다 우선순위가 낮은 애들한테는 양보 불가
+                                    // => 우선순위가 나보다 높거나 같은 쓰레드가 없으면 다시 본인한테옴
+                Thread.Yield();     // 관대한 양보 => 관대하게 양보할테니, 지금 실행이 가능한 쓰레드가 있으면 실행
+                                    // => 실행 가능한 애가 없으면 남은 시간 본인에게 소진
             }
 
             // 잘못된 예시
